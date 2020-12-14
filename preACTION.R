87fc9ce4c1f327d8a2a18a58bc4d2cd22c80b4b1
Eric <toeric@Eric-MacBook-Pro.local>
2020-12-14 14:00:16 +0800

    first

---
+++ preACTION.R
@@ -0,0 +1,26 @@
+args<-commandArgs(TRUE)
+require(ACTIONet)
+require(Seurat)
+require(SingleCellExperiment)
+require(scater)
+
+setwd(dirname(args[1]))
+print(sprintf("output folder: %s", dirname(args[1])))
+
+
+sc = readRDS(file.path(dirname(args[1]), args[1]))
+sc
+
+#sce = import.sce.from.Seurat(ace)
+
+sce <- as.SingleCellExperiment(sc)
+sce
+
+batch_attr = interaction(sce$source, sce$Phase)
+
+table(batch_attr)
+
+
+corrected.sce = reduce.and.batch.correct.ace.Harmony(sce, batch_attr = batch_attr)
+
+saveRDS(corrected.sce, file=sprintf("sce_%s_reducedim50_batchcorrected.rds",args[2]))
